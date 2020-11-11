9d38a5ed6c4a37d44209acab08db62e007e0749c
rohanphillips <rohan@solutions-insurance.com>
2020-11-10 20:00:45 -0600

    Done.

--- src/GraceHopperQuoteComponent.js
+++ src/GraceHopperQuoteComponent.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import App from './App';
 
 export class GraceHopperQuoteComponent extends Component {
 	render() {
@@ -15,3 +16,5 @@ export class GraceHopperQuoteComponent extends Component {
 		);
 	}
 }
+
+export default GraceHopperQuoteComponent;
